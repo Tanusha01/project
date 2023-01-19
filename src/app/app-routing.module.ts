ac81343b48c41236de89a50ce6421a52086715f1
Tanusha01 <tanya.polukoshko01@mail.ru>
2023-01-19 20:40:48 +0300

    Page layout About Us

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MyProjectComponent } from './my-project/my-project.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component:MyProjectComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MyProjectComponent } from './my-project/my-project.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyProjectComponent
   ],
   imports: [
     BrowserModule,
